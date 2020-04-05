7744df6c2404a69b7b0f8e33e19d65195ad073d4
bart4s <bart.w.zalecki@gmail.com>
2020-04-06 00:11:48 +0200

    method 2

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 @app.get("/method")
-def method():
-    return {"method": "METHOD"}
+def method_get():
+    return {"method": "GET"}
+
+@app.post("/method")
+def method_post():
+    return {"method": "POST"}
+
+@app.delete("/method")
+def method_delete():
+    return {"method": "DELETE"}
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
