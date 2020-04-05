7722ccec81dedffca7a167f5386ed796e9067f1b
bart4s <bart.w.zalecki@gmail.com>
2020-04-05 23:24:42 +0200

    method

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method")
+def method():
+    return {"method": "METHOD"}
