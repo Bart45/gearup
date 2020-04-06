25b53d0ce2249638b37fae5e248f1f17d63a80c5
bart4s <bart.w.zalecki@gmail.com>
2020-04-06 16:33:34 +0200

    hw 3 assign_patient_id implementation

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+from typing import Dict
+
 app = FastAPI()
+app.N = 0
+
+
+class AssignPatientIdRq(BaseModel):
+    name: str
+    surename: str
+
+
+class AssignPatientIdResp(BaseModel):
+    id: int
+    patient: Dict
+
 
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
+
 @app.get("/method")
 def method_get():
     return {"method": "GET"}
+
 
 @app.post("/method")
 def method_post():
     return {"method": "POST"}
 
+
 @app.delete("/method")
 def method_delete():
     return {"method": "DELETE"}
 
+
 @app.put("/method")
 def method_put():
     return {"method": "PUT"}
+
+
+@app.post("/patient", response_model=AssignPatientIdResp)
+def assign_patient_id(rq: AssignPatientIdRq):
+    app.N += 1
+    return AssignPatientIdResp(id=app.N, patient={"name": rq.name, "surename": rq.surename})
